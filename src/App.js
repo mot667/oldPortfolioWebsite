e44d11753a4f942287b4d87d7870957725a58e09
mot667 <t0m.k33ch@gmail.com>
2020-03-04 09:36:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,147 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { useOnClickOutside } from './hooks';
+import { GlobalStyles } from './global';
+import { theme } from './theme';
+import { Burger, Menu } from './components';
+import FocusLock from 'react-focus-lock';
 import './App.css';
+import { useDencrypt } from "use-dencrypt-effect";
+import ScrollNavigation from 'react-single-page-navigation';
+import ContactMe from './ContactMe';
+import { Parallax, Background } from 'react-parallax';
+
+
+
+const values = ["Not Your", "Average", "Software", "Engineer"];
+
+
+
+
+
+const Line1 = () => {
+  const { result, dencrypt } = useDencrypt();
+
+  React.useEffect(() => {
+    
+
+    const action = setInterval(() => {
+      dencrypt(values[0]);
+
+    }, 0);
+
+    return () => clearInterval(action);
+  }, []);
+
+  return <div style={{fontSize: 80}}>{result}</div>;
+};
+
+const Line2 = () => {
+  const { result, dencrypt } = useDencrypt();
+
+  React.useEffect(() => {
+    
+
+    const action = setInterval(() => {
+      dencrypt(values[1]);
+
+    }, 1000);
+
+    return () => clearInterval(action);
+  }, []);
+
+  return <div style={{fontSize: 80}}>{result}</div>;
+};
+
+const Line3 = () => {
+  const { result, dencrypt } = useDencrypt();
+
+  React.useEffect(() => {
+    
+
+    const action = setInterval(() => {
+      dencrypt(values[2]);
+
+    }, 2000);
+
+    return () => clearInterval(action);
+  }, []);
+
+  return <div style={{fontSize: 80}}>{result}</div>;
+};
+
+const Line4 = () => {
+  const { result, dencrypt } = useDencrypt();
+
+  React.useEffect(() => {
+    
+
+    const action = setInterval(() => {
+      dencrypt(values[3]);
+
+    }, 3000);
+
+    return () => clearInterval(action);
+  }, []);
+
+  return <div style={{fontSize: 80}}>{result}</div>;
+};
+
+
+
+
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  const menuId = "main-menu";
+
+  useOnClickOutside(node, () => setOpen(false));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+
+  <body>
+    <div className = "sectionStyle">
+      <div className = "centerDiv">
+      <Line1/>
+      <Line2/>
+      <Line3/>
+      <Line4/>
+      </div>
+      
+ 
+      
+      
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <div className = "menu" ref={node}>
+          <FocusLock disabled={!open}>
+            <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+            <Menu open={open} setOpen={setOpen} id={menuId} />
+          </FocusLock>
+        </div>
+
+        
+      </>
+     
+    </ThemeProvider>
     </div>
+   
+    </body>
+  
+
+  <div className = "ContactMe">
+     <ContactMe/>
+     </div>
+  
+     </div>
+  
+  
   );
 }
+
+
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+    primaryDark: '#0D0C1D',
+    primaryLight: '#EFFFFA',
+    primaryTransparent:'rgb(13, 69, 166,0.5)',
+    primaryHover: '#343078',
+    mobile: '576px',
+    
+  }
